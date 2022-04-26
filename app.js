e51e7e580190053df9b60717633531d00c7d50ee
kunbeg <goodkunal723@gmail.com>
2022-04-26 13:41:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const socket = require('socket.io')
+const app=express()//initialize
+app.use(express.static("public"))
+let port = 3000
+let server = app.listen(port,()=>{
+    console.log("Listening to port "+port);
+})
+
+let io = socket(server)
+io.on("connection",(socket)=>{
+    console.log("Made socket connection");
+    //recieved data
+    socket.on("beginPath",(data)=>{
+        // data -> from frontend
+        //now transfer data to all connector computers
+        io.sockets.emit("beginPath",data)
+    })
+    socket.on("drawStroke",(data)=>{
+        io.sockets.emit("drawStroke", data);
+    })
+    socket.on("redoUndo",(data)=>{
+        io.sockets.emit("redoUndo", data);
+    })
+})
